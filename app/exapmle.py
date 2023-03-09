d42ed8f2c33c5d8861baa8bcd587f99f51d4c811
Santiago Rosas <santiagorosasplaza@gmail.com>
2023-03-09 11:44:34 -0600

    nuevos archivos

---
+++ app/exapmle.py
@@ -0,0 +1,11 @@
+#####para poder ejecutar como modulo o como script un codigo sin importar como lo quieras usar es neceario hacer varias cosas
+
+import main 
+print(main.data) ##va ejeutar absolutamnete todo a pesard e que seleccione ñla variable  data por lo que hay que separaralos en diferentes funciones yu de esa manera que no se ejecute toodo entonces hay que dividirlo en varias funciones y llamarlas 
+
+###lo podemos separar tal como elejemplo main en donde ya se hizo la correcion para evitar esto a traves de funciones 
+print(main.data)
+main.run()
+
+
+ 
